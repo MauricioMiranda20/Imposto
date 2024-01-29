2fe27180e9d16cc44c725dae06b7dd3064d7067c
Mauricio Miranda <64110966+MauricioMiranda20@users.noreply.github.com>
2024-01-28 23:48:25 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		System.out.printf("Digite o valor do Imposto:");
 		double a = sc.nextDouble();
